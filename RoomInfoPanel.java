447f27a29e92d66b1edbd0b93072b9dad50d2422
SoeLTun <soelintun700@gmail.com>
2014-12-03 12:45:47 -0800

    Update RoomInfoPanel.java

--- RoomInfoPanel.java
+++ RoomInfoPanel.java
@@ -25,6 +25,7 @@ public class RoomInfoPanel extends JPanel implements ChangeListener
 {
 	  private Model model;
 	  private PrintReceiptFrame printFrame;
+	  
 	public RoomInfoPanel(Model m,final JFrame reservationframe, final JFrame welcomeframe)
 	{
 		 model =m;
@@ -32,6 +33,33 @@ public class RoomInfoPanel extends JPanel implements ChangeListener
 		 this.setLayout(new GridBagLayout());
 		 this.setBackground(new Color(79, 79, 82).brighter());
 		 this.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		 
+		 // create  labels for check-in/out
+		 JLabel checkinLabel = new JLabel("  Check-in:");
+		 JLabel checkoutLabel = new JLabel("  Check-out:");
+		 checkinLabel.setForeground(Color.black);
+		 checkoutLabel.setForeground(Color.black);
+		 checkinLabel.setPreferredSize(new Dimension(110,30));
+		 checkoutLabel.setPreferredSize(new Dimension(110,30));
+		 checkinLabel.setFont(new Font("Tahoma", Font.BOLD,14));
+		 checkoutLabel.setFont(new Font("Tahoma",Font.BOLD,14));
+		 
+		 // create  text field for check-in/out
+		 JTextField checkinTextField = new JTextField("mm/dd/yyyy");
+		 JTextField checkoutTextField = new JTextField("mm/dd/yyyy");
+		 checkinTextField.setPreferredSize(new Dimension(110,30));
+		 checkoutTextField.setPreferredSize(new Dimension(110,30));
+		 
+		 // create buttons for room type & label
+		  JLabel buttonTypeLabel = new JLabel("  Room Type:");
+		  buttonTypeLabel.setFont(new Font("Tahoma", Font.BOLD,16));
+		  JButton twoHundredBtn = new JButton("$200");
+		  JButton  oneHundredBtn = new JButton("$100");
+		  twoHundredBtn.setPreferredSize(new Dimension (90,40));
+		  twoHundredBtn.setFont(new Font("Tahoma",Font.BOLD,14));
+		  oneHundredBtn.setPreferredSize(new Dimension (90,40));
+		  oneHundredBtn.setFont(new Font("Tahoma",Font.BOLD,14));
+		 
 		
 		 // create test area with scroll bar for room info
 		 JTextArea roominfoTextArea = new JTextArea(12,23);
@@ -39,7 +67,7 @@ public class RoomInfoPanel extends JPanel implements ChangeListener
 		 
 		 
 		 // create  label for panel
-		 JLabel titleLabel = new JLabel("     Available Rooms");
+		 JLabel titleLabel = new JLabel("   Available Rooms");
 		 titleLabel.setForeground(Color.blue);
 		 titleLabel.setAlignmentX(CENTER_ALIGNMENT);
 		 titleLabel.setPreferredSize(new Dimension(230,50));
@@ -54,30 +82,63 @@ public class RoomInfoPanel extends JPanel implements ChangeListener
 		 transactionDoneBtn.setFont(new Font("Tahoma",Font.BOLD,14));
 		 
 		 // add component to this panel
+		  GridBagConstraints c = new GridBagConstraints();
 		 
-		 GridBagConstraints c = new GridBagConstraints();
-		 c.fill =  GridBagConstraints.VERTICAL;
-		 c.weightx = 1;
-		 c.gridy = 0;
-		 c.gridx = 0;
+		  c.gridx = 1;
+		  c.gridy = 0;
+		  this.add(checkinLabel,c);
+		
+		  c.gridx = 2;
+		  c.gridy = 0;
+		this.add(checkoutLabel,c);
+		
+		  c.gridx = 1;
+		  c.gridy = 1;
+		 this.add(checkinTextField,c);
+		 
+		  c.gridx = 2;
+		  c.gridy = 1;
+		 this.add(checkoutTextField,c);
+		 
+		  c.gridx = 0;
+		  c.gridy = 2;
+		 this.add(buttonTypeLabel,c);
+		 
+		  c.gridx = 1;
+		  c.gridy = 2;
+		 this.add(twoHundredBtn,c);
+		 
+		  c.gridx = 2;
+		  c.gridy = 2;
+		 this.add(oneHundredBtn,c);
+		 
+		 
+		 c.gridx = 1;
+		 c.gridy = 3;
 		 this.add(titleLabel,c);
 		
-		 c.weighty = 1;
-		 c.gridy = 1;
+		 c.fill =  GridBagConstraints.HORIZONTAL;
+		 c.gridy = 4;
 		 c.gridx =0;
-		 c.insets = new Insets(0,2,0,0);
+		 c.gridwidth = 3;
 		
 		this.add(roominfoScrollPane,c);
 		
 		c.anchor  = GridBagConstraints.BELOW_BASELINE_TRAILING;
-		c.fill =  GridBagConstraints.HORIZONTAL;
-		 c.gridy = 5;
-		 c.gridx = 1;
+		
+		 c.gridy = 6;
+		 c.gridx = 4;
 		 this.add(confirmBtn, c);
 		 
-		 c.gridy = 5;
-		 c.gridx = 2;
+		 c.insets = new Insets(0,2,0,0);
+		 c.gridy = 6;
+		 c.gridx = 7;
 		 this.add(transactionDoneBtn,c);
+		 
+		 
+		 
+		  
+		
 		 
 		 //TODO add ActionListener to Buttons
 			
